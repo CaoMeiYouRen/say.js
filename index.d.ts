9e20a346befed8f5c9905a002fc32fb15077ee63
CaoMeiYouRen <996881204@qq.com>
2025-01-21 20:05:29 +0800

    fix: 更新 getInstalledVoices 方法的回调类型以支持错误和声音列表

--- index.d.ts
+++ index.d.ts
@@ -4,11 +4,13 @@ declare module 'say' {
   namespace SayJS {
     type errorCallback = (err: string) => void;
 
+    type getInstalledVoicesErrorCallback = (err: string, voices: string[]) => void;
+
     class Say {
       public export(text: string, voice?: string, speed?: number, filePath?: string, callback?: errorCallback): void;
       public speak(text: string, voice?: string, speed?: number, callback?: errorCallback): void;
       public stop(): void;
-      public getInstalledVoices(callback: errorCallback): void;
+      public getInstalledVoices(callback: getInstalledVoicesErrorCallback): void;
     }
   }
 
